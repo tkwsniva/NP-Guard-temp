23016eb741e36cfbc6a9d5186e09fa1661c6c2e9
tkwsniva <98817881+tkwsniva@users.noreply.github.com>
2022-02-20 14:22:35 +0200

    Initial commit

---
+++ _plugins/encodeBase64.rb
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------------------
+# ~/_plugins/encodeBase64.rb
+# Liquid filter for J1 Template to Base64 encode
+#
+# Product/Info:
+# http://jekyll.one
+#
+# Copyright (C) 2021 Juergen Adams
+#
+# J1 Template is licensed under the MIT License.
+# See: https://github.com/jekyll-one-org/J1 Template/blob/master/LICENSE
+# ------------------------------------------------------------------------------
+#  NOTE:
+#  CustomFilters cannot be used in SAFE mode (e.g not usable for
+#  rendering pages with Jekyll on GitHub
+#
+#  USAGE:
+#    {% capture cache_name %}
+#
+#      liquid code to generate HTML output goes here
+#
+#    {% endcapture %}
+#    {{ cache_name | encode64 }}
+#
+# ------------------------------------------------------------------------------
+# noinspection SpellCheckingInspection
+# rubocop:disable Lint/UnneededDisable
+# rubocop:disable Style/Documentation
+# rubocop:disable Metrics/LineLength
+# rubocop:disable Layout/TrailingWhitespace
+# rubocop:disable Layout/SpaceAroundOperators
+# rubocop:disable Layout/ExtraSpacing
+# rubocop:disable Metrics/AbcSize
+# ------------------------------------------------------------------------------
+require 'base64'
+
+module Jekyll
+  module EncodeBase64
+    def encodeBase64(input)
+      encoded = Base64.encode64(input)
+      input = encoded
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::EncodeBase64)
